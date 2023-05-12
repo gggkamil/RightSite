fbbe9a16c5a303b8d5feed2b2d4f49ccd6164fa4
gggkamil <74270259+gggkamil@users.noreply.github.com>
2023-05-13 00:35:34 +0200

    mail

---
+++ Pages/ProcessRequest.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Web;
+namespace RightSite.Pages
+
+{
+    public class ProcessRequestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
